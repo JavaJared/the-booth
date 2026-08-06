2ca3768b0e3244bcf039dd7a6ac5d59933adbd25
JavaJared <141364428+JavaJared@users.noreply.github.com>
2026-08-06 11:30:57 -0400

    Add firebase config

---
+++ public/firebase-config.example.js
@@ -0,0 +1,10 @@
+// Copy to firebase-config.js and paste your web app's config.
+// Project settings -> General -> Your apps -> Web app -> SDK setup.
+export const firebaseConfig = {
+  apiKey: '...',
+  authDomain: '....firebaseapp.com',
+  projectId: '...',
+  storageBucket: '....appspot.com',
+  messagingSenderId: '...',
+  appId: '...',
+};
