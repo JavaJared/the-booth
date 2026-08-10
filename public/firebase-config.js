cafb7726559ceb558069f24ed475d0fdf82986aa
JavaJared <141364428+JavaJared@users.noreply.github.com>
2026-08-10 10:24:12 -0400

    Create firebase-config.js

---
+++ public/firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA7UwzBEsNHAM6jnT8uBeMhBaqXwFJoPdA",
+  authDomain: "booth-battle-74829.firebaseapp.com",
+  projectId: "booth-battle-74829",
+  storageBucket: "booth-battle-74829.firebasestorage.app",
+  messagingSenderId: "657025786897",
+  appId: "1:657025786897:web:c575dbd7f51e4f39517db0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
